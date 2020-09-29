e72b4fe009a98d3af0958612b9446d21c4899904
Emily <emily.mape.98@gmail.com>
2020-09-29 15:51:32 -0500

    Paso por Valor tipos primitivos
    Creacion de Clases
    Uso de this
    Constructores y sobrecarga de constructores
    Alcance de variables
    Paso por valor tipos primitivos

---
+++ Fundamentos/CreacionClases/src/PasoPorValor/PasoPorValor.java
@@ -0,0 +1,20 @@
+package PasoPorValor;
+
+public class PasoPorValor {
+    public static void main(String[] args) {
+        //Como es un metodo static los metodos que se llamen tiene que ser static
+        var x = 10;
+        System.out.println("x = " + x);
+        //Se pasa una copia del valor de x
+        cambioValor(x);
+        //Aun con el cambio de valor x se queda intacta
+        System.out.println("x nuevo valor = " + x);
+        
+    }
+    //El paso por valor es solo con tipos primitivos
+    public static void cambioValor(int arg1){
+        System.out.println("arg1 = " + arg1);
+        //Solo modifica el valor de la varaible que recibe la copia de x
+        arg1 = 15;
+    }
+}
