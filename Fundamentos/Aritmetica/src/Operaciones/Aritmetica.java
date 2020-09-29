43ba253eb0674c25453f523019277c6992a00330
Emily <emily.mape.98@gmail.com>
2020-09-29 14:50:32 -0500

    Constructores y Sobrecarga de constructores de clases

--- Fundamentos/Aritmetica/src/Operaciones/Aritmetica.java
+++ Fundamentos/Aritmetica/src/Operaciones/Aritmetica.java
@@ -5,6 +5,23 @@ public class Aritmetica {
     int a; //Valor por default de int es 0
     int b;
     
+    //Constructores
+    //Un constructor tiene el mismo nombre de la clase
+    //Solo se llama cuando se crea el objeto (con el uso de 'new')
+    public Aritmetica(){
+        System.out.println("Ejecuntando constructor");
+    }
+    
+    //La sobrecarga de constructores es tener varios constructores
+    //La diferencia de un constructor a otro son los argumentos que reciven
+    //var no se puede usar para recibir argumentos en un metodo
+    public Aritmetica(int a, int b){
+        //Con ctrl click sobre una variable nos lleva a su definicion
+        this.a = a;
+        this.b = b;
+        System.out.println("Constructor con argumentos");
+    }
+    
     //Metodo
     public void sumar(){
         int resultado = a + b;
--- Fundamentos/Aritmetica/src/Operaciones/PruebaAritmetica.java
+++ Fundamentos/Aritmetica/src/Operaciones/PruebaAritmetica.java
@@ -3,7 +3,7 @@ package Operaciones;
 public class PruebaAritmetica {
     //Un metodo estatico no tiene acceso al operador this
     public static void main(String[] args) {
-        //Crear instancia
+        //Crear instancia con el uso de contructor
         Aritmetica aritmetica1 = new Aritmetica();
         aritmetica1.a = 3;
         aritmetica1.b = 2;
@@ -14,5 +14,17 @@ public class PruebaAritmetica {
         
         resultado = aritmetica1.sumarConArgumentos(5, 8);
         System.out.println("resultado usando argumentos = " + resultado);
+        
+        //Usando constructor vacio
+        Aritmetica aritmetica2 = new Aritmetica();
+        //Imprimir atributos de nuestra instancia
+        System.out.println("aritmetica2 a: " + aritmetica2.a);
+        System.out.println("aritmetica2 b: " + aritmetica2.b);
+        
+        //Usando constructor con argumentos
+        Aritmetica aritmetica3 = new Aritmetica(5, 8);
+        System.out.println("aritmetica3 a: " + aritmetica3.a);
+        System.out.println("aritmetica3 b: " + aritmetica3.b);
+        
     }
 }
